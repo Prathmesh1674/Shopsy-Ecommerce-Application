bbae3d2b4e2d8acd47034be1d3fdd18a31f79e06
Prathmesh1674 <ps1320084@gmail.com>
2026-08-27 19:25:41 +0530

    Fix frontend backend URL

--- Ecomm-UI/js/regester.js
+++ Ecomm-UI/js/regester.js
@@ -1,4 +1,4 @@
-/*const BASE_URL = "http://localhost:8080";
+/*
 
 document
     .getElementById("registerForm")
@@ -60,7 +60,7 @@ async function registerUser(e) {
 
 }*/
 
-const BASE_URL = "http://localhost:8080";
+
 
 document
     .getElementById("registerForm")
