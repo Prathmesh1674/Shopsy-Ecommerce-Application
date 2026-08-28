1cb91b0d5e7d5801fcd223313f1312b1e8497b0e
Prathmesh1674 <ps1320084@gmail.com>
2026-08-28 10:43:09 +0530

    Allow Render frontend in CORS

--- Ecomm/src/main/java/com/sher/Ecomm/config/SecurityConfig.java
+++ Ecomm/src/main/java/com/sher/Ecomm/config/SecurityConfig.java
@@ -105,7 +105,8 @@ public class SecurityConfig {
                                     "http://127.0.0.1:5500",
                                     "http://127.0.0.1:5501",
                                     "http://localhost:5500",
-                                    "http://localhost:5501"
+                                    "http://localhost:5501",
+                                    "https://shopsy-frontend-diaj.onrender.com"
                             )
                     );
 
